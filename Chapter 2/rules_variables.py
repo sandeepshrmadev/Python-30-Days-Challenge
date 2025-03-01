2641df9a35aa6a8c7e9d08503dd6e8b2d7b1ceaf
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmadev@gmail.com>
2025-03-01 14:48:32 +0530

    Up to date </>

---
+++ Chapter 2/rules_variables.py
@@ -0,0 +1,12 @@
+a = 23
+
+aaa = 435
+
+sandeep = 34
+
+sandeep = 45
+
+_sandeep = 34
+
+# @sandeepr = 56 # Invalid due to @ symbol
+# s@ndeep # Invalid due to @ symbol
---
+++ Chapter 2/type.py
@@ -0,0 +1,10 @@
+a = 32.2
+
+b = "Sandeep"
+
+t = type(a)
+
+e = type(b)
+
+print(t)
+print(e)
