8418d67ad15ba71c78d48b2ea0ed961555bb1160
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmadev@gmail.com>
2025-03-01 14:45:34 +0530

    Add files via upload

--- Chapter 1/Practice/problem1.py
+++ Chapter 1/Practice/problem1.py
@@ -1,1 +1,10 @@
-
+print('''demon slayer pain
+feel free like an eagle
+too not ever yield back
+is always a promise to this self
+from pain to pure strength
+for a slayer fiercely waiting there I
+to even slice an enormous boulder
+that gets in my way
+so don't you dare
+come close to the ones I care''
