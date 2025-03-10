c5294141d5dc1b506d628111cba3a719d925a984
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmadev@gmail.com>
2025-03-10 21:36:45 +0530

    Done </>

--- Chapter 9/file.py
+++ Chapter 9/file.py
@@ -1,1 +1,11 @@
+'''
+a = "asus"
 
+emails = []
+3 seconds
+'''
+
+f = open("file.txt", "r")
+data = f.read()
+print(data)
+f.close()
---
+++ Chapter 9/file_write.py
@@ -0,0 +1,7 @@
+st = "Hey Sandeep you are amazing"
+
+f = open("myfile.txt", "w")
+
+f.write(st)
+
+f.close()
